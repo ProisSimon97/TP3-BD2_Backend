1b23275d627e36c8e223a70d9aab748f983a2b78
ProisSimon97 <simonviedma3@gmail.com>
2023-09-09 18:29:37 -0300

    first commit

---
+++ src/main/java/ar/unrn/tp/web/ClienteController.java
@@ -0,0 +1,34 @@
+package ar.unrn.tp.web;
+
+import ar.unrn.tp.api.ClienteService;
+import ar.unrn.tp.dto.TarjetaDto;
+import ar.unrn.tp.modelo.Tarjeta;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<List<TarjetaDto>> obtenerTarjetasDeCliente(@NotNull Long id) {
+        List<Tarjeta> tarjetas = clienteService.listarTarjetas(id);
+
+        List<TarjetaDto> response = tarjetas.stream()
+                .map(TarjetaDto::fromDomain)
+                .toList();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
